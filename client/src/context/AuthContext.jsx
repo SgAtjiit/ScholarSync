b30ef3e460d15cb8b3f64308853bfcdda6fc5072
SgAtjiit <shrishpankajguptadbd6@gmail.com>
2025-11-30 22:45:45 +0530

    updated scopes

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = useGoogleLogin({
     flow: 'auth-code',
-    scope: 'https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/drive.file email profile openid',
+    scope: 'https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.student-submissions.me.readonly https://www.googleapis.com/auth/drive.file email profile openid',
     prompt: 'consent',
     onSuccess: async ({ code }) => {
       try {
