962f400a71b185ca643c4ed56b355c260ca65805
SgAtjiit <shrishpankajguptadbd6@gmail.com>
2025-12-05 22:58:26 +0530

    feat: implement AuthContext for Google login/logout and user session management.

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -23,10 +23,10 @@ export const AuthProvider = ({ children }) => {
     }
     setLoading(false);
   }, []);
-//resolved scope error
+  //resolved scope error
   const login = useGoogleLogin({
     flow: 'auth-code',
-    scope: 'https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.student-submissions.me.readonly https://www.googleapis.com/auth/drive.file email profile openid',
+    scope: 'https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.student-submissions.me.readonly https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly email profile openid',
     prompt: 'consent',
     onSuccess: async ({ code }) => {
       try {
